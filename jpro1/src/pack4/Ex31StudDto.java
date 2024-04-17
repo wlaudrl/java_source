5ad5396d994bbd35d616a6e6634bf1a10f46e1f4
mg <wlaudrl6940@gmail.com>
2024-04-17 17:25:08 +0900

    101010

--- jpro1/src/pack4/Ex31StudDto.java
+++ jpro1/src/pack4/Ex31StudDto.java
@@ -1,43 +1,19 @@
 package pack4;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+//@Getter 
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+@Data
 public class Ex31StudDto {
 	private String name;
 	private int kor, eng;
 	
-	public Ex31StudDto() {
-		
-	}
-	
-	public Ex31StudDto(String name, int kor, int eng) {
-		this.name = name;
-		this.kor = kor;
-		this.eng = eng;
-		
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getKor() {
-		return kor;
-	}
-
-	public void setKor(int kor) {
-		this.kor = kor;
-	}
-
-	public int getEng() {
-		return eng;
-	}
-
-	public void setEng(int eng) {
-		this.eng = eng;
-	}
-	
-
 }
---
+++ jpro1/src/pack4/Ex32HaksaengDto.java
@@ -0,0 +1,14 @@
+package pack4;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ex32HaksaengDto {
+	private String name;
+	private int kor, eng, mat;
+
+}
